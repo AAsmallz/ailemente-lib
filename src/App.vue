66b8d26cf16db63138cf10b4d88d38c569f4af43
aasmallz <2474069068@qq.com>
2022-02-27 22:20:18 +0800

    feat: 设计基础组件设计-布局Container组件

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <!-- <el-container>
+    <el-header>Header</el-header>
+    <el-main>Main</el-main>
+    <el-footer>Footer</el-footer>
+  </el-container> -->
+  <el-container>
+    <el-header>Header</el-header>
+    <el-container>
+      <el-aside width="200px"> Aside </el-aside>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
+</template>
+<script lang="ts" setup></script>
+<style lang="scss">
+body {
+  width: 1000px;
+  margin: 10px auto;
+}
+.el-header,
+.el-footer {
+  background-color: pink;
+}
+.el-aside {
+  background-color: brown;
+}
+.el-main {
+  background-color: aqua;
+}
+</style>
